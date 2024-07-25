51f6d081ea4ae181b1662ef30c11acf11b770b89
Shaik Fayaz <sfayaz@IN-SFAYAZ-MBP.local>
2024-07-26 01:35:38 +0530

    Week2 starter files

--- week1/src/main/java/com/Backend/week1/Week1Application.java
+++ week1/src/main/java/com/Backend/week1/Week1Application.java
@@ -1,13 +1,27 @@
 package com.Backend.week1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Week1Application {
 
+
+
+@SpringBootApplication
+public class Week1Application implements CommandLineRunner {
+
+	@Autowired
+	Apple objec;
 	public static void main(String[] args) {
 		SpringApplication.run(Week1Application.class, args);
+		//setObjec();
+
+	//destroy();
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		objec.setApple();
+	}
 }
